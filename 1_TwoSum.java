8a2ef2113fdad11accd712777abef0fcb10472ab
tunveyyy <panditanvi@gmail.com>
2020-08-05 23:59:19 -0400

    LeetCode 1

---
+++ 1_TwoSum.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] op = new int[2];
+        
+        for(int i = 0; i < nums.length; i++) {
+            
+            if(map.containsKey(nums[i])) {
+                op[0] = i;
+                op[1] = map.get(nums[i]);
+            }
+            else{
+                map.put(target - nums[i], i);
+            }
+            
+        }
+        return op;
+    }
+}
